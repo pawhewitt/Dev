05f51ccd27bc015af7457d58c0f7337a5302983d
phewitt <pawhewitt@gmail.com>
2018-02-01 19:18:48 +0000

    1 Feb 2018

--- configure_shell.sh
+++ configure_shell.sh
@@ -1,1 +1,4 @@
-./configure prefix=/usr/local/SU2_CST --disable-CFD --disable-GEO --disable-IDE --disable-MSH --disable-SOL --disable-DOT --disable-DEF
+#./configure prefix=/usr/local/SU2_CST --disable-CFD --disable-GEO --disable-IDE --disable-MSH --disable-SOL --disable-DOT --disable-DEF
+
+./configure prefix=/usr/local/SU2_CST --disable-GEO --disable-IDE --disable-MSH --disable-SOL --disable-DOT --disable-DEF
+
